6f2bf33d57410c2d2057f0d268c96a5458d002a0
valeria <a.valeria.koriatchenko@freenet.de>
2018-05-14 15:09:19 +0200

    Add all specs

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,12 +31,13 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-
+        it("should have a (defined) URL that is not empty")
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        it("should have a (defined) name that is not empty")
     });
 
 
